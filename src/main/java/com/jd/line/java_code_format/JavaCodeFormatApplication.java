7c68339be56a8e1edebff8e65b27edd4c1054ab9
rongran <rongran@jd.com>
2021-08-05 18:31:21 +0800

    init

---
+++ src/main/java/com/jd/line/java_code_format/JavaCodeFormatApplication.java
@@ -0,0 +1,13 @@
+package com.jd.line.java_code_format;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeFormatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeFormatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jd/line/java_code_format/JavaCodeFormatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.line.java_code_format;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeFormatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
